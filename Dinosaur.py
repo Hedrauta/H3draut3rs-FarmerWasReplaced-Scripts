95cf216ff1fb16164ea4a6d9e7fdf98084fb11a8
H3draut3r <20142298+Hedrauta@users.noreply.github.com>
2025-10-25 02:13:30 +0200

    Dinosaur needs complete rework

--- Dinosaur.py
+++ Dinosaur.py
@@ -1,33 +1,17 @@
-#Todo: rework, keeps breaking
+#Todo: rework, keeps breaking, remove set, add "avoid-loops" depending on length on first rows
 from __builtins__ import *
 from helper import *
 change_hat(Hats.Traffic_Cone)
 goto(0,0)
 wosi = get_world_size()
 change_hat(Hats.Dinosaur_Hat)
-bones = set() # coord-list unsorted, can been "searched"
-bone_list = [] # coord-list in order, remove first entry, if len > bone_length, append new location
 bone_length = 2 # Start with 2 "possible bones", increase by one everytime apple been eaten
-
-def update(x,y):
-	if len(bones) >= bone_length:
-		old_bone = bone_list[0]
-		bones.remove(old_bone)
-		bone_list.pop(0)
-	bones.add((x,y))
-	bone_list.append((x,y))
-
-def smove(dir, step2 = False):
-		if step2:
-			move(dir)
-			update(get_pos_x(), get_pos_y())
-		move(dir)
-		update(get_pos_x(), get_pos_y())
 
 applex, appley = 0,0
 mx, my = get_pos_x(), get_pos_y()
 on_apple = False
 
+#rework starts here:
 while True:
 	if (applex == mx and appley == my):
 		applex, appley = measure()
