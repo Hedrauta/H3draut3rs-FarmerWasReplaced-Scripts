41f626d2e37dda6e66f679e50863304c9f169426
H3draut3r <20142298+Hedrauta@users.noreply.github.com>
2025-10-26 01:28:41 +0200

    Small adjustments

--- Dinosaur.py
+++ Dinosaur.py
@@ -1,5 +1,3 @@
-# Todo: rework, keeps breaking, remove set, add "avoid-loops" 
-#	depending on length on first rows
 from __builtins__ import *
 from helper import *
 change_hat(Hats.Brown_Hat)
@@ -11,16 +9,11 @@ tail_length = 2 # Start with 1 "tail + head", increase by one everytime apple be
 applex, appley = 0,0
 mx, my = get_pos_x(), get_pos_y()
 on_apple = False
-# min length avoid: 124  (( math: 2*wosi+2*(wosi-2))) > math for loop : (min_length - 60 - 2*wosi) // 60 
-# 
-#rework starts here:
+
 while True:
 	applex, appley = measure()
 	on_apple = False
 	while not on_apple:
-		#debug:
-		if applex == 1 and (appley+1)%2 and mx == 1 and (appley-my) <=3:
-			pass
 		# avoid-loop:
 		loops = ((tail_length -(2 * wosi)) // 60) - (my / 2)
 		if mx == 1 and (my+1) % 2 and loops > 0 and not on_apple:
@@ -39,16 +32,12 @@ while True:
 			break
 		#snake-logic:
 		if (my+1) % 2 and mx == 1 and (appley-2 >= my or appley < my) and my < 30 and not on_apple:
-			# quick_print((mx,my),(applex,appley),(appley-2 >= my or appley < my),appley-2 >= my,appley < my)
 			move(North)
 			mx, my = get_pos_x(), get_pos_y()
 			on_apple = applex == mx and appley == my
 			yup = move(North)
 			if not yup and not on_apple:
 				move(East)
-			else:
-				mx, my = get_pos_x(), get_pos_y()
-				on_apple = applex == mx and appley == my
 		elif mx == 0 and my > 0 and not on_apple:
 			move(South)
 		elif (my +1 ) % 2 and not on_apple:
@@ -72,3 +61,5 @@ while True:
 	tail_length += 1
 	if (wosi**2) == tail_length:
 		break
+#harvest():
+change_hat(Hats.Brown_Hat)
