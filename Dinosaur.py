ee9b47be20a1d65d7b86cfd945be5930e055d707
H3draut3r <20142298+Hedrauta@users.noreply.github.com>
2025-10-26 00:20:51 +0200

    New Dinosaur logic
    Completes in average 28 mins

--- Dinosaur.py
+++ Dinosaur.py
@@ -1,40 +1,74 @@
-#Todo: rework, keeps breaking, remove set, add "avoid-loops" depending on length on first rows
+# Todo: rework, keeps breaking, remove set, add "avoid-loops" 
+#	depending on length on first rows
 from __builtins__ import *
 from helper import *
-change_hat(Hats.Traffic_Cone)
+change_hat(Hats.Brown_Hat)
 goto(0,0)
 wosi = get_world_size()
 change_hat(Hats.Dinosaur_Hat)
-bone_length = 2 # Start with 2 "possible bones", increase by one everytime apple been eaten
+tail_length = 2 # Start with 1 "tail + head", increase by one everytime apple been eaten
 
 applex, appley = 0,0
 mx, my = get_pos_x(), get_pos_y()
 on_apple = False
-
+# min length avoid: 124  (( math: 2*wosi+2*(wosi-2))) > math for loop : (min_length - 60 - 2*wosi) // 60 
+# 
 #rework starts here:
 while True:
-	if (applex == mx and appley == my):
-		applex, appley = measure()
-	while applex != mx or appley != my:
-		if (mx == 1 and (my+1) % 2 and my+2 < wosi and not (mx,my+2) in bones) and (appley >= my+2 or appley < my) and not on_apple:
-			smove(North, True)
-		elif (mx == 0) and (my > 0) and not on_apple:
-			smove(South)
-		elif ((my+1) % 2 and not on_apple):
-			if appley < my and mx == 1 and not (mx,my+2) in bones:
-				smove(North)
-			elif (mx == wosi - 1):
-				smove(North)
+	applex, appley = measure()
+	on_apple = False
+	while not on_apple:
+		#debug:
+		if applex == 1 and (appley+1)%2 and mx == 1 and (appley-my) <=3:
+			pass
+		# avoid-loop:
+		loops = ((tail_length -(2 * wosi)) // 60) - (my / 2)
+		if mx == 1 and (my+1) % 2 and loops > 0 and not on_apple:
+			for i in range(loops):
+				if appley < my+2:
+					break
+				xmove(East, wosi-2)
+				move(North)
+				xmove(West, wosi-2)
+				move(North)
+				mx,my = get_pos_x(), get_pos_y()
+				on_apple = applex == mx and appley == my
+			mx, my = get_pos_x(), get_pos_y()
+			on_apple = applex == mx and appley == my
+		if on_apple:
+			break
+		#snake-logic:
+		if (my+1) % 2 and mx == 1 and (appley-2 >= my or appley < my) and my < 30 and not on_apple:
+			# quick_print((mx,my),(applex,appley),(appley-2 >= my or appley < my),appley-2 >= my,appley < my)
+			move(North)
+			mx, my = get_pos_x(), get_pos_y()
+			on_apple = applex == mx and appley == my
+			yup = move(North)
+			if not yup and not on_apple:
+				move(East)
 			else:
-				smove(East)
+				mx, my = get_pos_x(), get_pos_y()
+				on_apple = applex == mx and appley == my
+		elif mx == 0 and my > 0 and not on_apple:
+			move(South)
+		elif (my +1 ) % 2 and not on_apple:
+			if appley < my and mx == 1:
+				yup = move(North)
+				if not yup:
+					move(East)
+			elif mx == wosi - 1:
+				move(North)
+			else:
+				move(East)
 		elif not on_apple:
-			if mx > 1 and my <= wosi-1:
-				smove(West)
-			elif mx == wosi - 1 or (my < wosi - 1 and mx == 1):
-				smove(North)
+			if mx > 1 and my % 2:
+				move(West)
+			elif mx == 1 and my < wosi - 1:
+				move(North)
 			else:
-				smove(West)
-		
+				move(West)
 		mx, my = get_pos_x(), get_pos_y()
 		on_apple = applex == mx and appley == my
-	bone_length += 1
+	tail_length += 1
+	if (wosi**2) == tail_length:
+		break
